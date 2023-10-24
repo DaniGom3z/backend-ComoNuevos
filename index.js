ab1e2e2dd80afe8cb83bb87eb179e4df100e2f1f
DaniGom3z <danielhernandezgomez103@gmail.com>
2023-10-24 17:37:28 -0600

    rama develop

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const usuariosRouter = require('./src/routes/usuarios.route');
 const administradorRouter = require('./src/routes/administrador.route'); 
 
 
-// Importamos la configuración de MySQL y creamos la conexión
 const db = require('./src/config/db.config');
 
 // Middleware para procesar JSON
 app.use(express.json());
 
-// Rutas para administrador y usuarios
+
 app.use('/administrador', administradorRouter);
 app.use('/usuarios', usuariosRouter);
 
-// Iniciamos el servidor
+
 app.listen(PORT, () => {
     console.log(`API escuchando en el puerto ${process.env.PORT}`);
 });
