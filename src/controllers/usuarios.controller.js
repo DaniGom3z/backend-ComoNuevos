4dfff88bf0601051dba1c37813d6cdad639c4840
DaniGom3z <danielhernandezgomez103@gmail.com>
2023-10-29 07:33:14 -0600

    confirmacion de citas

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,6 +1,7 @@
-const validator = require('validator');
-const Auto = require("../models/auto.model");
-const Cita = require("../models/cita.model");
+const nodemailer = require('nodemailer');
+const validator = require("validator");
+const Auto = require('../models/auto.model');
+const Cita = require('../models/cita.model');
 
 const obtenerAutos = async (req, res) => {
   try {
@@ -15,18 +16,45 @@ const agregarCita = async (req, res) => {
   try {
     const { nombre, correo, dia } = req.body;
 
-    if (typeof correo !== "string" || !validator.isEmail(correo)) {
-      return res.status(400).json({ error: "Correo electrónico no válido" });
+    if (typeof correo !== 'string' || !validator.isEmail(correo)) {
+      return res.status(400).json({ error: 'Correo electrónico no válido' });
     }
 
     const citaExistente = await Cita.findOne({ where: { correo } });
     if (citaExistente) {
       return res
         .status(400)
-        .json({ error: "El correo ya está registrado en la tabla de citas" });
+        .json({ error: 'El correo ya está registrado en la tabla de citas' });
     }
 
     const nuevaCita = await Cita.create({ nombre, correo, dia });
+
+    // Configura el servicio de correo
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail', // Cambia esto por tu proveedor de correo
+      auth: {
+        user: 'comonuevoschiapas@gmail.com', // Cambia esto por tu correo
+        pass: 'xvhc uqqg gbtj ursk', // Cambia esto por tu contraseña
+      },
+    });
+
+    // Opciones para enviar el correo
+    const mailOptions = {
+      from: 'comonuevoschiapas@gmail.com', // Remitente
+      to: correo, // Destinatario (correo del usuario)
+      subject: 'Confirmación de cita',
+      text:'Hola ' + nombre + ',\nTu cita ha sido confirmada con éxito. Gracias por agendar con nosotros el día ' + dia + '.\nTe estaremos esperando con mucho gusto.',
+    };
+
+    // Envía el correo electrónico
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error al enviar el correo:', error);
+      } else {
+        console.log('Correo enviado:', info.response);
+      }
+    });
+
     res.json(nuevaCita);
   } catch (err) {
     res.status(500).json({ error: err.message });
--- src/routes/administrador.route.js
+++ src/routes/administrador.route.js
@@ -3,6 +3,7 @@ const administradorRoute = express.Router();
 const protegerRutas = require('../middlewares/protegerRutas');
 const { crearUsuario } = require("../controllers/crearUsuario.controller");
 const { iniciarSesion} = require("../controllers/login.controller");
+const { cerrarSesion } = require("../controllers/cerrarSesion.controller")
 const {
   obtenerAutos,
   ingresarAuto,
@@ -23,5 +24,6 @@ administradorRoute.put("/autos/recuperar/:id_auto", protegerRutas, recuperarAuto
 administradorRoute.delete("/autos/:id_auto", protegerRutas, eliminarAuto);
 administradorRoute.get("/citas", protegerRutas, obtenerCitas);
 administradorRoute.delete("/citas/:id_cita", protegerRutas, eliminarCita);
+administradorRoute.delete("/cerrarSesion", protegerRutas, cerrarSesion);
 
 module.exports = administradorRoute;
